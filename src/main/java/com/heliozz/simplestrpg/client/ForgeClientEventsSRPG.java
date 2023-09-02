dfafbfb95d1a68a9eb5d589d29ed2a674e11d2a5
heliozz10 <helioa31415@gmail.com>
2023-09-03 01:01:00 +0600

    Did stuff

--- src/main/java/com/heliozz/simplestrpg/client/ForgeClientEventsSRPG.java
+++ src/main/java/com/heliozz/simplestrpg/client/ForgeClientEventsSRPG.java
@@ -21,22 +21,21 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(value = Dist.CLIENT, modid = SimplestRPG.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ForgeClientEventsSRPG {
 	private static final Logger LOGGER = LogUtils.getLogger();
+	private static Minecraft minecraft = Minecraft.getInstance();
 	
 	@SubscribeEvent
 	public static void onLevelRenderTest(RenderLevelStageEvent event) {
 		if(event.getStage() == RenderLevelStageEvent.Stage.AFTER_SKY) {
 			Vector3f view = event.getCamera().getPosition().toVector3f();
-			Minecraft minecraft = Minecraft.getInstance();
 			float realTicks = minecraft.getPartialTick() + minecraft.level.getGameTime();
-			LOGGER.info(Float.toString(realTicks));
 			PoseStack poseStack = event.getPoseStack();
 			poseStack.pushPose();
 			MultiBufferSource.BufferSource bufferSource = Minecraft.getInstance().renderBuffers().bufferSource();
 			VertexConsumer vertexConsumer = bufferSource.getBuffer(RenderType.lines());
-			Vector3f pos = new Vector3f(0.0F, 100.5F, 0.0F);
-			ClientUtil.lineFromTo(view, poseStack, vertexConsumer, 
-					pos, new Vector3f(pos).add((float) Math.cos(2.0D * Math.PI * ((double)realTicks/20.0D) - Math.PI/2), 0.0F, (float) Math.sin(2.0D * Math.PI * ((double)realTicks/20.0D)- Math.PI/2)), 
-					new Vector4f(1.0F, 0.0F, 0.0F, 1.0F), new Vector4f(0.0F, 0.0F, 1.0F, 1.0F));
+			Vector3f start = new Vector3f(0.0F, 100.0F, 0.0F);
+			Vector3f next = new Vector3f(1.0F, 101.0F, 0.0F);
+			Vector4f color = new Vector4f(1.0F, 0.0F, 0.0F, 1.0F);
+			ClientUtil.lineFromTo(view, poseStack, vertexConsumer, start, next, color, color);
 			poseStack.popPose();
 		}
 	}
